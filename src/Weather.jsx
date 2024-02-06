04a29def0c0dd3e875ab8614481612431b82d340
Moyanka Das <dasmoyanka@gmail.com>
2024-02-06 11:53:17 +0530

    Add commit

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,6 +1,7 @@
 import Input from "./Input.jsx";
 import Output from "./Output.jsx";
 import {useState} from 'react';
+import "./Weather.css";
 
 export default function Weather(){
     let [cityInfo,setCityInfo]=useState({
@@ -17,7 +18,7 @@ export default function Weather(){
         setCityInfo(city);
     }
     return (
-        <div style={{backgroundImage:"url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE3Klx5d8Liw8Wtd-iKK8wRaji_nUKUTf_snar54r0yA&s')",backgroundSize:"cover",fontFamily:"Gill Sans,Gill Sans MT,Calibri,Trebuchet MS,sans-serif"}}>
+        <div className="main">
             <Input handleCityInfo={handleCityInfo}/>
             <Output city={cityInfo}/>
         </div>
